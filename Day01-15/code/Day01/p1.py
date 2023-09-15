96e09f1d0f24c4a2bb582bc4417501c92e41d54d
1iuhuikang <armorsmith.bp@gmail.com>
2023-09-15 15:02:15 +0800

    Day 1 Practice

---
+++ Day01-15/code/Day01/p1.py
@@ -0,0 +1,45 @@
+import this
+
+"""
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+"""
+Python之禅 Tim Peters
+
+美优于丑
+简明优于晦涩
+简单优于复杂
+复杂优于复杂到爆炸
+扁平优于嵌套
+间隔优于拥挤
+代码要具有可读性
+情况再特殊，也不至于特殊到破坏原则
+实践优于理论
+不要放过任何一个错误，除非你刻意为之
+面对未知和歧义，别掷骰子搁那猜
+有且只有一件事该去做：找到正确做法
+现在开始做比永远不做好
+如果你的方案很难解释，坏方案
+如果你的方案很好解释，也许好
+命名空间很好，多用
+"""
