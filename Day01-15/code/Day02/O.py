4afd99a1981029df42a3bfd9b266061a46470e99
1iuhuikang <armorsmith.bp@gmail.com>
2023-09-25 16:02:55 +0800

    Day 02

---
+++ Day01-15/code/Day02/O.py
@@ -0,0 +1,10 @@
+r = float(input('r = ? : '))
+
+perimeter = 2 * 3.1416 * r
+
+area = 3.1416 * r * r
+
+print("perimeter = %.2f" % perimeter)
+print("area = %.2f" % area)
+
+
---
+++ Day01-15/code/Day02/trans.py
@@ -0,0 +1,7 @@
+f = float(input('give me a number:'))
+
+c = (f-32) / 1.8
+
+print('%.1f华氏度 = %.1f摄氏度' % (f,c))
+
+ 
